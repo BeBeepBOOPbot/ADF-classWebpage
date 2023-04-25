1b4b012035def9c21ea1cb5e7e753cb822ad6db6
NishitaaaS <130098185+NishitaaaS@users.noreply.github.com>
2023-04-25 16:54:06 -0400

    new changes

--- gallery.js
+++ gallery.js
@@ -3,11 +3,10 @@ const observer = new IntersectionObserver((entries) => {
         console.log(entry)
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
-            entry.target.classList.remove('hidden');
             
+
         } else {
             entry.target.classList.remove('show');
-            entry.target.classList.add('hidden');
         }
     });
 });
