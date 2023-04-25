168fa9e6a255b31fd841ffa4139df12f9d8a3659
NishitaaaS <130098185+NishitaaaS@users.noreply.github.com>
2023-04-25 16:26:30 -0400

    Changes

---
+++ Assets/javascript/gallery.js
@@ -0,0 +1,16 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+            entry.target.classList.remove('hidden');
+        } else {
+            entry.target.classList.remove('show');
+            entry.target.classList.add('hidden');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
