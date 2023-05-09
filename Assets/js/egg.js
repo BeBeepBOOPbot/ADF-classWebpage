3ae65ec22104a9af69b7435c059bf8d64c260e9a
Yreva1795 <averythomas04@gmail.com>
2023-05-09 09:40:11 -0400

    Easter Egg - DONT LOOK AT CONTENTS OF FILE

---
+++ Assets/js/egg.js
@@ -0,0 +1,23 @@
+var paragraph = document.getElementById("egg");
+paragraph.textContent += "Thankyou Mr.Strunk for all you have done for us, we will always remember you as an amazing teacher";
+
+var paragraph = document.getElementById("egg1");
+paragraph.textContent += "Mr. Strunk is the most experienced, kind, and caring teacher I've ever had. His experience is something I wish to have and I know this is the start of that journey. Mr. Strunk has changed my life for the better, and I hope that fact stays with him for the rest of his life. I'll remember him for the rest of mine. - Aiden";
+
+var paragraph = document.getElementById("egg2");
+paragraph.textContent += "Mr strunk is easily one of the best teachers I had or ever will have. Even though I managed to learn more about css than him he still was about to teach me and help me learn and achieve more than I thought I could. I will remember and keep the lessons you taught me always. - Avery";
+
+var paragraph = document.getElementById("egg3");
+paragraph.textContent += "Number one, Mr Strunk always delivers. Whether it's teaching the course, assisting me with outside problems, or giving the class donuts. Mr Strunk is honestly too nice, it's almost suspicious. Joking, M Strunk is one of my favorite teachers and he is also good looking. - abdu";
+
+var paragraph = document.getElementById("egg4");
+paragraph.textContent += "Mr.Strunk is not only an amazing teacher but also a great human being. He always goes the extra mile, doing nice things like having donuts to celebrate our passing tests or learning about and getting our class evolved in a wiring project, that would provide us with valuable practical experience, which was beyond the circulum. In addition to that, Mr. Strunk is also very funny, extremely kind, and passionate about what he is teaching. It was a pleasure being in his class this year. I appreciate his much needed mentorship and thoughtful guidance that he provided us with. Thank you Mr. Strunk! - Nishita";
+
+var paragraph = document.getElementById("egg5");
+paragraph.textContent += "Hey Mr strunk, It's kye I wanted to thank you so so much for such a wonderful year with our class I met new friends and so many fun memories. I loved all the fun projects we did this year and all the times when you would award us with donuts and chips, it was just amazing. I will miss being in APP development with such a wonderful class and I couldn't ask for a better teacher. Thank you so much Mr strunk! -kye";
+
+var paragraph = document.getElementById("egg6");
+paragraph.textContent += "6";
+
+var paragraph = document.getElementById("egg7");
+paragraph.textContent += "7";
